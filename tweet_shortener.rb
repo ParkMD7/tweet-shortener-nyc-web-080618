a828e39ad87003aff006654aeeea66958d2ac46e
Parker Dinsmore <parkermdinsmore@gmail.com>
2018-07-26 15:03:36 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -3,7 +3,7 @@
 def dictionary(tweets)
   passengers = {
     hello: "hi",
-    suite_b: "Seymour Hoffman",
+    to, two, too: "2",
     suite_c: "Alfred Tennyson",
     suite_d: "Charlie Chaplin",
     suite_e: "Crumpet the Elf"
